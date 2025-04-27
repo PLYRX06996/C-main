b4a6e5148f6df4e4daa568b9afef03eb93b3bf70
PLYRX06996 <abdulqadirmemon2104@gmail.com>
2025-04-27 19:12:29 +0530

    commiting murder🤣

--- C++/CodeWithHarris/tut41.cpp
+++ C++/CodeWithHarris/tut41.cpp
@@ -11,8 +11,25 @@ class Base1 {
     public:
     Base1(int a) : base1Var(a) {}
 };
-
+class Base2 {
+    protected:
+    int base2Var;
+    public:
+    Base2(int a) : base2Var(a) {}
+};
+class derived : public Base1, public Base2 {
+    protected:
+    int derVar;
+    public:
+    void show() {
+        cout << "The base-variable-01 is: " << base1Var <<  endl;
+        cout << "The base-variable-02 is: " << base2Var <<  endl;
+        cout << "The sum of base-variables is: " << base1Var + base2Var <<  endl;
+    }
+};
 int main() {
+    derived d1;
+    
 
     return 0;
 }
